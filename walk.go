304a8993198a784dbd36e19bbe8246bb34554bee
matt <mattlemmone@gmail.co>
2020-01-13 22:16:36 -0800

    fix filter logic

--- walk.go
+++ walk.go
@@ -1,74 +1,78 @@
 package main
 
 import (
-	"fmt"
-	"os"
-	"path/filepath"
-	"strings"
+    "fmt"
+    "os"
+    "path/filepath"
+    "strings"
 )
 
-const maxFileWalkDepth = 7
+const maxFileWalkDepth = 6
 
 var blacklistedDirs = map[string]bool{
-	"build": true,
+    "build": true,
+    "node_modules": true,
 }
 
 func discoverPaths() []string {
-	var paths []string
+    var paths []string
 
-	cwd, err := os.Getwd()
+    cwd, err := os.Getwd()
 
-	if err != nil {
-		fmt.Println(err.Error())
-	}
+    if err != nil {
+        fmt.Println(err.Error())
+    }
 
-	filepath.Walk(
-		cwd,
-		func(path string, fileInfo os.FileInfo, err error) error {
-			if err != nil {
-				fmt.Println(err.Error())
-				return err
-			}
+    filepath.Walk(
+        cwd,
+        func(path string, fileInfo os.FileInfo, err error) error {
+            if err != nil {
+                fmt.Println(err.Error())
+                return err
+            }
 
-			if !fileInfo.IsDir() {
-				return nil
-			}
+            if !fileInfo.IsDir() {
+                return nil
+            }
 
-			if shouldSkipDirectory(fileInfo, path) {
-				return filepath.SkipDir
-			}
+            if shouldSkipDirectory(fileInfo, path) {
+                return filepath.SkipDir
+            }
 
-			paths = append(paths, path)
+            paths = append(paths, path)
 
-			return nil
-		},
-	)
+            return nil
+        },
+    )
 
-	return paths
+    fmt.Println(paths)
+
+    return paths
 }
 
 func shouldSkipDirectory(fileInfo os.FileInfo, path string) bool {
-	isDotFile := strings.HasPrefix(fileInfo.Name(), ".")
+    isDotFile := strings.HasPrefix(fileInfo.Name(), ".")
+    exceedsMaxFolderDepth := strings.Count(path, "/") > maxFileWalkDepth
 
-	if isDotFile {
-		return true
-	}
+    if isDotFile || exceedsMaxFolderDepth || isBlacklisted(path) {
+        return true
+    }
 
-	exceedsMaxFolderDepth := strings.Count(path, "/") > maxFileWalkDepth
+    return false
+}
 
-	if exceedsMaxFolderDepth {
-		return true
-	}
+func isBlacklisted(path string) bool {
+    fileNameIdx := strings.LastIndex( path,"/")
 
-	fileNameIdx := strings.LastIndex("/", path)
+    if fileNameIdx == -1 {
+        return false
+    }
 
-	if fileNameIdx != -1 {
-		fileName := path[fileNameIdx+1:]
+    fileName := path[fileNameIdx+1:]
 
-		if _, ok := blacklistedDirs[fileName]; ok {
-			return true
-		}
-	}
+    if _, ok := blacklistedDirs[fileName]; ok {
+        return true
+    }
 
-	return false
+    return false
 }
